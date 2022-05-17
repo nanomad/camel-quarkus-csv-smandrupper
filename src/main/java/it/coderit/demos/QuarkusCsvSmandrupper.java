4dddd7d2e850d317c58924fc669d9827932113ff
Giovanni Condello <giovanni.condello@coderit.it>
2022-05-17 19:55:17 +0200

    First revision

---
+++ src/main/java/it/coderit/demos/QuarkusCsvSmandrupper.java
@@ -0,0 +1,57 @@
+package it.coderit.demos;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.AggregationStrategies;
+import org.apache.camel.builder.ExpressionBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.csv.CsvConstants;
+import org.apache.camel.dataformat.csv.CsvDataFormat;
+import org.apache.camel.language.simple.SimpleExpressionBuilder;
+import org.apache.commons.csv.CSVFormat;
+
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+@Singleton
+public class QuarkusCsvSmandrupper extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        CsvDataFormat csv = new CsvDataFormat()
+                .setFormat(CSVFormat.EXCEL)
+                .setCaptureHeaderRecord(true)
+                .setUseOrderedMaps(true)
+                .setDelimiter(';');
+
+        from("file://temp?include=.*\\.csv")
+                .log("Received file: ${file:name}")
+                .streamCaching()
+                .unmarshal(csv)
+                .split(SimpleExpressionBuilder.collateExpression("${body}", 50)).streaming().aggregationStrategy(AggregationStrategies.useOriginal())
+                    .to("direct:process-bucket")
+                .end()
+                .log("Completed processing file: ${file:name}");
+
+        from("direct:process-bucket")
+                .setHeader(Exchange.FILE_NAME, simple("${file:onlyname.noext}-${header.CamelSplitIndex}.csv"))
+                .split(body(), AggregationStrategies.flexible().accumulateInCollection(ArrayList.class))
+                .process(x -> {
+                    Map<String, Object> body = x.getMessage().getBody(Map.class);
+                    float total = Float.parseFloat((String) body.get("price")) * Float.parseFloat((String) body.get("qty"));
+                    body.put("total", total);
+                })
+                .end()
+                .process(x -> {
+                    List<String> header = x.getMessage().getHeader(CsvConstants.HEADER_RECORD, List.class);
+                    Map<String, Object> headerAsMap = new LinkedHashMap<>(header.size());
+                    header.forEach(e -> headerAsMap.put(e, e));
+                    List<Map<String, Object>> rows = x.getMessage().getBody(List.class);
+                    rows.add(0, headerAsMap);
+                })
+                .marshal(csv)
+                .to("file://temp/out")
+                .log("Generated ${header.CamelFileName}");
+    }
+}
